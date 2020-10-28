8d884a47649a868cbb2684290dfe7c23f012a456
1ahmadsam <samiahmad360@hotmail.com>
2020-10-28 04:35:19 -0400

    commiting starter code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Link } from 'react-scroll';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <nav>
+        {/* <a href='#page1'>page 1</a>
+        <a href='#page2'>page 2</a>
+        <a href='#page3'>page 3</a> */}
+        <Link to='page1' smooth={true}>
+          p1
+        </Link>
+        <Link to='page2' smooth={true}>
+          p2
+        </Link>
+        <Link to='page3' smooth={true}>
+          p3
+        </Link>
+      </nav>
+      <section id='page1'>
+        <img
+          src='https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg'
+          alt=''
+        />
+      </section>
+      <section id='page2'>
+        <img
+          src='https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d'
+          alt=''
+        />
+      </section>
+      <section id='page3'>
+        <img
+          src='https://api.timeforkids.com/wp-content/uploads/2020/04/200410016385.jpg'
+          alt=''
+        />
+      </section>
     </div>
   );
 }
